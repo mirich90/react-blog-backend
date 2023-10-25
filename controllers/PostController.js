db19a2f9f5d8e6ffc75945d9657d8077ce28ab75
genkoskarev <gen.koskarev@gmail.com>
2023-10-25 17:41:46 +0300

    add tags

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -12,6 +12,23 @@ export const getAll = async (req, res) => {
   }
 };
 
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(6).exec();
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
+    res.json(tags);
+  } catch (error) {
+    console.warn(error);
+    res.status(500).json({
+      message: "Не удалось получить статьи",
+    });
+  }
+};
+
 export const getOne = async (req, res) => {
   try {
     const postId = req.params.id;
